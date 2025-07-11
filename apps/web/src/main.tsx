532d8dad6339ad9f02fc850247856d940b77a672
floklein <4128942+floklein@users.noreply.github.com>
2025-07-11 21:33:34 +0200

    biome

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,9 @@
-import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
 import ReactDOM from "react-dom/client";
 import Loader from "./components/loader";
 import { routeTree } from "./routeTree.gen";
-
-  import { QueryClientProvider } from "@tanstack/react-query";
-  import { queryClient, trpc } from "./utils/trpc";
+import { queryClient, trpc } from "./utils/trpc";
 
 const router = createRouter({
   routeTree,
@@ -13,9 +12,7 @@ const router = createRouter({
   context: { trpc, queryClient },
   Wrap: function WrapComponent({ children }: { children: React.ReactNode }) {
     return (
-      <QueryClientProvider client={queryClient}>
-        {children}
-      </QueryClientProvider>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     );
   },
 });
