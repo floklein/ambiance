c67bb20743bd6c611ecd2ff7e2e69e934f107487
floklein <4128942+floklein@users.noreply.github.com>
2025-07-12 04:54:03 +0200

    poc

--- apps/server/src/routers/index.ts
+++ apps/server/src/routers/index.ts
@@ -1,14 +1,77 @@
+import { sounds } from "@ambiance/sounds";
+import OpenAI from "openai";
+import { z } from "zod";
 import { protectedProcedure, publicProcedure, router } from "../lib/trpc";
+
+const openai = new OpenAI({
+  baseURL: "https://openrouter.ai/api/v1",
+  apiKey: process.env.OPENROUTER_API_KEY,
+});
+
+const SYSTEM_PROMPT = `
+You are a helpful assistant that can play sounds.
+You will be given a message and you will need to play the sound that best matches the message.
+The user can tell a story, or ask directly for a sound.
+Do not make up any sounds, only choose from the list.
+Do not answer the user's message, only play the sound.
+Do not answer any words, only play the sound.
+
+Here is a list of sounds to choose from, formatted as "- [soundId] soundName (soundDescription)":
+${Object.entries(sounds)
+  .map(
+    ([soundId, sound]) => `- [${soundId}] ${sound.name} (${sound.description})`,
+  )
+  .join("\n")}
+`;
 
 export const appRouter = router({
   healthCheck: publicProcedure.query(() => {
     return "OK";
   }),
-  privateData: protectedProcedure.query(({ ctx }) => {
-    return {
-      message: "This is private",
-      user: ctx.session.user,
-    };
-  }),
+  getSound: protectedProcedure
+    .input(
+      z.object({
+        message: z.string(),
+      }),
+    )
+    .mutation(async ({ input }) => {
+      try {
+        const completion = await openai.chat.completions.create({
+          model: "openai/gpt-4.1-nano",
+          tools: [
+            {
+              type: "function",
+              function: {
+                name: "playSound",
+                description: "Play a sound",
+                parameters: {
+                  type: "object",
+                  properties: {
+                    soundId: {
+                      type: "string",
+                      enum: Object.keys(sounds),
+                      description: "The ID of the sound to play",
+                    },
+                  },
+                  required: ["soundId"],
+                },
+              },
+            },
+          ],
+          messages: [
+            {
+              role: "system",
+              content: SYSTEM_PROMPT,
+            },
+            { role: "user", content: input.message },
+          ],
+        });
+        return completion.choices[0].message;
+      } catch (error) {
+        console.error(JSON.stringify(error, null, 2));
+        throw error;
+      }
+    }),
 });
+
 export type AppRouter = typeof appRouter;
--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,7 +1,10 @@
-import { sounds } from "@ambiance/sounds";
+import { type Sound, sounds } from "@ambiance/sounds";
+import { useMutation } from "@tanstack/react-query";
 import { createFileRoute, redirect } from "@tanstack/react-router";
+import { useRef, useState } from "react";
 import { Textarea } from "@/components/ui/textarea";
 import { authClient } from "@/lib/auth-client";
+import { trpc } from "@/utils/trpc";
 
 export const Route = createFileRoute("/")({
   component: HomeComponent,
@@ -16,6 +19,32 @@ export const Route = createFileRoute("/")({
 });
 
 function HomeComponent() {
+  const [message, setMessage] = useState("");
+  const [sound, setSound] = useState<Sound | null>(null);
+
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  const { mutate: getSound } = useMutation(
+    trpc.getSound.mutationOptions({
+      onSuccess: (data) => {
+        console.log(data);
+        data.tool_calls?.forEach((toolCall) => {
+          if (toolCall.function.name === "playSound") {
+            const { soundId } = JSON.parse(toolCall.function.arguments);
+            const sound = sounds[soundId];
+            console.log(sound);
+            if (sound) {
+              setSound(sound);
+              setTimeout(() => {
+                audioRef.current?.play();
+              }, 100);
+            }
+          }
+        });
+      },
+    }),
+  );
+
   return (
     <div className="flex h-svh flex-col items-center justify-center">
       <div className="flex w-full max-w-3xl flex-col gap-4">
@@ -23,11 +52,24 @@ function HomeComponent() {
           Change the{" "}
           <span className="font-serif text-primary italic">Ambiance...</span>
         </h1>
-        <Textarea autoFocus className="resize-none shadow-2xl" />
+        <Textarea
+          autoFocus
+          className="resize-none shadow-2xl"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter" && !e.shiftKey) {
+              e.preventDefault();
+              getSound({ message });
+            }
+          }}
+        />
       </div>
-      <audio controls>
-        <source src={sounds.goodhaven.url} />
-      </audio>
+      {sound && (
+        <audio ref={audioRef} controls>
+          <source src={sound.url} />
+        </audio>
+      )}
     </div>
   );
 }
