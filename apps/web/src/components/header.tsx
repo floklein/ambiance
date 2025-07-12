9e1c2f3f55c49065216ed4029b245d38ed2d3ea9
floklein <4128942+floklein@users.noreply.github.com>
2025-07-12 16:33:37 +0200

    ui 3

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -5,11 +5,11 @@ export default function Header() {
   return (
     <div>
       <div className="fixed top-0 right-0 left-0 flex h-16 select-none items-center justify-center">
-        <h1 className="font-bold font-mono text-2xl text-primary tracking-[1em]">
+        <h1 className="indent-[1em] font-bold font-mono text-2xl text-primary tracking-[1em]">
           AMBIANCE
         </h1>
       </div>
-      <div className="fixed top-0 right-0 flex h-16 flex-row items-center gap-2 px-3.5">
+      <div className="fixed top-0 right-0 flex h-16 flex-row items-center px-3.5">
         <ModeToggle />
         <UserMenu />
       </div>
