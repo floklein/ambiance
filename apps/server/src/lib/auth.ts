18d1e2ef6167434dfaa1727e64d8d8bff5313eaf
floklein <4128942+floklein@users.noreply.github.com>
2025-07-13 06:33:04 +0200

    google

--- apps/server/src/lib/auth.ts
+++ apps/server/src/lib/auth.ts
@@ -6,13 +6,17 @@ import * as schema from "../db/schema/auth";
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
-
     schema: schema,
   }),
   trustedOrigins: [process.env.CORS_ORIGIN || ""],
-  emailAndPassword: {
-    enabled: true,
-  },
   secret: process.env.BETTER_AUTH_SECRET,
   baseURL: process.env.BETTER_AUTH_URL,
+  socialProviders: {
+    google: {
+      prompt: "select_account",
+      redirectURI: process.env.GOOGLE_REDIRECT_URI || "",
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    },
+  },
 });
